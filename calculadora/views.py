e9abb40a05232498cc4bd104157b270126c114e3
Igor Dias <igorverissimo02@hotmail.com>
2022-01-10 20:26:26 -0300

    adicionado busca de alimentos

--- calculadora/views.py
+++ calculadora/views.py
@@ -4,9 +4,48 @@ import os.path
 
 import pandas
 
+BASE = os.path.dirname(os.path.abspath(__file__))
+arquivo = pandas.read_csv(os.path.join(BASE, 'static/taco-db-nutrientes.csv'))
+
+
 def index(request):
-    BASE = os.path.dirname(os.path.abspath(__file__))
-    df = pandas.read_csv(os.path.join(BASE, 'static/taco-db-nutrientes.csv'), encoding='UTF-8', sep=';')
+
+    search = request.GET.get('search')
+    alimentos,nutrientes = pequisarAlimento(search,['proteína (g)'])
+   
+    context = {'titulo' : 'Kcalculator','alimentos':alimentos}   
     
-    context = {'titulo' : 'Kcalculator'}
+   
     return render(request, 'calculadora/index.html', context)
+
+def obterNutrientes(alimento,nutrientes):
+  alimento = alimento.lower()
+  alimento = arquivo['nome']==alimento
+  listaDeNutrientes=[]
+  if (type(nutrientes)==list):
+    for i in range(len(nutrientes)):
+      listaDeNutrientes.append(float(arquivo[alimento][nutrientes[i]]))
+  else:
+    listaDeNutrientes.append(float(arquivo[alimento][nutrientes]))
+  
+  return listaDeNutrientes
+
+def pequisarAlimento(alimento, nutrientes ):
+  nomeDosAlimentos = []
+  listaNutrientes = []
+  if(alimento):
+    alimento = alimento.lower()
+    listaDeAlimentos = []
+    for i in arquivo['nome']:
+      if alimento in i:
+        listaDeAlimentos.append(True)
+      else:
+        listaDeAlimentos.append(False)
+    listaDeAlimentos=arquivo[listaDeAlimentos]
+   
+    for i in range(len(listaDeAlimentos)):
+
+      nomeDosAlimentos.append(listaDeAlimentos.iloc[i]['nome'])
+      listaNutrientes.append(obterNutrientes(nomeDosAlimentos[i],nutrientes))
+  
+  return nomeDosAlimentos, listaNutrientes
