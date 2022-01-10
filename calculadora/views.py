b9ee5788fa93110c654c295dddcd43a7ae2ab50a
VitorFelipePatricio <vitor.felipeee88@gmail.com>
2022-01-10 19:37:19 -0300

    Configuração do pandas

--- calculadora/views.py
+++ calculadora/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.template import context
+
+import os.path
+
+import pandas
 
 def index(request):
+    BASE = os.path.dirname(os.path.abspath(__file__))
+    df = pandas.read_csv(os.path.join(BASE, 'static/taco-db-nutrientes.csv'), encoding='UTF-8', sep=';')
+    
     context = {'titulo' : 'Kcalculator'}
     return render(request, 'calculadora/index.html', context)
